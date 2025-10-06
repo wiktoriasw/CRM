00bf6753441af144ca5232f5de72ce20c94d7df7
wiktoriasw <wiktoriaswiech.ws@gmail.com>
2025-10-06 15:12:50 +0200

    Add tasks.py

---
+++ tasks.py
@@ -0,0 +1,28 @@
+from invoke import task
+
+
+@task
+def lint(c):
+    c.run("python -m black .")
+    c.run("python -m isort .")
+    c.run("python -m flake8")
+
+
+# @task
+# def populate(c):
+#     c.run("python populate_database.py")
+
+
+# @task
+# def run(c):
+#     c.run("fastapi dev main.py")
+
+
+# @task
+# def reqs(c):
+#     c.run("pip install -r requirements.txt")
+
+
+# @task
+# def test(c):
+#     c.run("pytest")
